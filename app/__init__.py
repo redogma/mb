2bd8f43e1187786985ba34414c067c4530289ba8
idx1008 <idx1008@gmail.com>
2018-08-27 13:13:36 +0100

    mail support

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from config import Config
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -15,6 +17,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
